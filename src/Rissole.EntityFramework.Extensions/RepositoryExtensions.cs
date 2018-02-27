31cd60d8d0d48975f8c68c2c50ffd48314060782
jiarong.gu <jiarong.gu@infotrack.com.au>
2018-02-27 22:34:27 +1100

    1. Added pdb files 2. Added repository extensions

---
+++ src/Rissole.EntityFramework.Extensions/RepositoryExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rissole.EntityFramework.Extensions
+{
+    public static class RepositoryExtensions
+    {
+        public static Lazy<IGenericRepository<TEntity>> GetLazyRepository<TEntity>(this DbSet<TEntity> dbSet) where TEntity : class
+        {
+            return new Lazy<IGenericRepository<TEntity>>(() => new GenericRepository<TEntity>(dbSet));
+        }
+    }
+}
