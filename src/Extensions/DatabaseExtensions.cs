2e2ab8ee553afa73c339d8e78ff4130beb1f5097
jiarong.gu <jiarong.gu@infotrack.com.au>
2018-02-27 17:04:37 +1100

    extension completed package nupkg config

---
+++ src/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace Extensions
+{
+    public static class DatabaseExtensions
+    {
+        public static void EnsureMigrate(this DatabaseFacade databaseFacade)
+        {
+            databaseFacade.CreateDatabase();
+            databaseFacade.CreateHistoryTable();
+            databaseFacade.Migrate();
+        }
+
+        public static void CreateDatabase(this DatabaseFacade databaseFacade)
+        {
+            using (var dbConnection = databaseFacade.GetDbConnection())
+            {
+                if (string.IsNullOrEmpty(dbConnection.Database))
+                    throw new NoNullAllowedException("database must be defined in connection");
+
+                var database = dbConnection.Database;
+
+                dbConnection.ClearDatabase();
+                dbConnection.Open();
+
+                string script = $"{ScriptConstants.CreateDatabaseIfNotExist} {database}";
+                dbConnection.ExecuteNonQuery(script);
+            }
+        }
+
+        public static void CreateHistoryTable(this DatabaseFacade databaseFacade)
+        {
+            using (var dbConnection = databaseFacade.GetDbConnection())
+            {
+                dbConnection.Open();
+
+                string script = $"{ScriptConstants.CreateTableIfNotExist} {ScriptConstants.MigrationsHistoryTable}";
+                dbConnection.ExecuteNonQuery(script);
+            }
+        }
+
+        private static void ClearDatabase(this IDbConnection dbConnection)
+        {
+            var connectionString = string.Join(";", dbConnection.ConnectionString.Split(';')
+                .Select(x => x.Split('=').Select(y => y.Trim()).ToList())
+                .Where(x => !x[0].Equals("database", StringComparison.CurrentCultureIgnoreCase))
+                .Select(x => $"{x[0]}={x[1]}"));
+
+            dbConnection.ConnectionString = connectionString;
+        }
+
+        private static void ExecuteNonQuery(this IDbConnection connection, string script)
+        {
+            using (IDbCommand command = connection.CreateCommand())
+            {
+                command.CommandText = script;
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ src/Extensions/ScriptConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Extensions
+{
+    internal static class ScriptConstants
+    {
+        public const string MigrationsHistoryTable = "__EFMigrationsHistory (MigrationId nvarchar(150) NOT NULL, ProductVersion nvarchar(32) NOT NULL, PRIMARY KEY(MigrationId))";
+        public const string CreateDatabaseIfNotExist = "CREATE DATABASE IF NOT EXISTS";
+        public const string CreateTableIfNotExist = "CREATE TABLE IF NOT EXISTS";
+    }
+}
